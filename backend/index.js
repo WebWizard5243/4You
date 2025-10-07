7997906ef61d15911d01ef7a5c5d4f79ddabe5e6
Kaif <kaif@kaifs-MacBook-Air.local>
2025-10-07 15:15:43 +0530

    fixing google login

--- backend/index.js
+++ backend/index.js
@@ -224,7 +224,7 @@ app.post("/serviceProvider",async(req,res) => {
        const Details = req.body.Details;
         const service = req.body.serviceProvided;
         try{
-            const result = await pool.query("SELECT * FROM service_providers WHERE companyName = $1",[companyName]);
+            const result = await pool.query("SELECT * FROM service_providers WHERE companyname = $1",[companyName]);
             if(result.rows.length > 0){
                 res.render("ServiceProviderForm",{
                     error : "company already exists"
@@ -242,7 +242,7 @@ app.post("/serviceProvider",async(req,res) => {
 passport.use("google", new GoogleStrategy({
     clientID : process.env.GOOGLE_CLIENT_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL : "http://localhost:3000/auth/google/callback",
+    callbackURL : "https://fouryou-0vrf.onrender.com//auth/google/callback",
     userProfileURL : "https://www.googleapis.com/oauth2/v3/userinfo"
 }, async (accessToken, refreshToken, profile, cb) => {
   console.log(profile); 
